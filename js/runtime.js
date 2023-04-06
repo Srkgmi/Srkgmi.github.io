f8c6531d2f5b3550b74a4fd19b95aef19f931bde
Srkgmi <lc8795@126.com>
2023-04-07 00:06:31 +0800

    Site updated: 2023-04-07 00:06:31

--- js/runtime.js
+++ js/runtime.js
@@ -1,7 +1,4 @@
 var now=new Date;function createtime(){now.setTime(now.getTime()+1e3);
-var e=new Date("08/01/2022 00:00:00"),
-t=Math.trunc(234e8+(now-e)/1e3*17),
-a=(t/1496e5).toFixed(6),
 o=new Date("2023/04/05 06:07:08"),
 n=(now-o)/1e3/60/60/24,r=Math.floor(n),
 i=(now-o)/1e3/60/60-24*r,
